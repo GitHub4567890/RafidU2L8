1d8596d02c9d230f40608ef423d002c94b0725f5
Ibtehaz Rafid <IbtehazR@nycstudents.net>
2022-10-20 12:10:31 -0400

    2

--- src/Example2.java
+++ src/Example2.java
@@ -2,6 +2,14 @@ public class Example2
 {
     public static void main(String[] args)
     {
+//        Double i = 2.3; // Double and int needed
+//        Integer d = 5;
+//        System.out.println( d.intValue() ); //Assign proper variables / Add parenthesis
+//        System.out.println( i.doubleValue() );
+//        // Print out the min and max values possible for integers
+//        System.out.println(Integer.MIN_VALUE); // Capitalize min value / Add underscore
+//        System.out.println( Integer.MAX_VALUE ); // Change int to Integer / Remove parenthesis
+
         Integer d = 5;
         System.out.println(d.toString() + 5);
     }
